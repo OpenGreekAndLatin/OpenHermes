5b95cca9bcc9deafb194f07d856ac6676fabb560
Thibault Clérice <leponteineptique@gmail.com>
2014-12-09 14:00:03 +0100

    PEP8 compliance

--- Analysis/process.py
+++ Analysis/process.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys, os
+import sys
+import os
 import pickle
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
@@ -9,96 +10,103 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 from Corpus.dictionaries import Shelf
 from Analysis.computation import Computation
 
+
 class OpenSynonyms(object):
-	def __init__(self, corpus, algorythm):
-		if not issubclass(corpus, (Shelf)):
-			raise TypeError("Corpus is not a subclass of Corpus.dictionaries.Shelf")
+    def __init__(self, corpus, algorythm):
+        if not issubclass(corpus, (Shelf)):
+            raise TypeError("Corpus is not a subclass of Corpus.dictionaries.Shelf")
 
-		if not issubclass(algorythm, (Computation)):
-			raise TypeError("algorythm is not a subclass of Analysis.computation.Computation")
+        if not issubclass(algorythm, (Computation)):
+            raise TypeError("algorythm is not a subclass of Analysis.computation.Computation")
 
-		self.shelf = corpus
-		self.corpus = self.shelf()
-		self.algorythm = algorythm
+        self.shelf = corpus
+        self.corpus = self.shelf()
+        self.algorythm = algorythm
 
-		self.path = os.path.dirname(os.path.abspath(__file__))
+        self.path = os.path.dirname(os.path.abspath(__file__))
 
-	def search(self, POS = "N", lemma = "bonus"):
-		if not self.results:
-			self.analyse()
-		if POS not in self.results:
-			raise ValueError("This PartOfSpeach is not available in this Corpus")
+    def search(self, POS, lemma):
 
-		if lemma not in self.results[POS].index.values:
-			raise ValueError("{0} is not in the values of {1}".format(lemma, POS))
+        if not isinstance(POS, str):
+            raise TypeError("POS should be a string, {0} given".format(type(POS)))
 
-		found = self.results[POS]
-		found = found.sort(lemma, ascending=True)
-		found = found[lemma]
-		found = found[found > 0.0]
-		found_subset = found[1:10]
-		heads = found_subset.keys().tolist()
-		found_subset = found_subset.tolist()
+        if not isinstance(lemma, str):
+            raise TypeError("lemma should be a string, {0} given".format(type(lemma)))
 
-		return list(zip(heads, found_subset))
+        if not self.results:
+            self.analyse()
+        if POS not in self.results:
+            raise ValueError("This PartOfSpeach is not available in this Corpus")
 
-	def checkCacheAnalyse(self):
-		return False
+        if lemma not in self.results[POS].index.values:
+            raise ValueError("{0} is not in the values of {1}".format(lemma, POS))
 
-	def generate(self, force = False):
-		""" Read and Generate the dictionaries """
-		data = {}
-		for lang in self.corpus.data:
-			data[lang] = self.corpus.data[lang].convert(force = force)
-		self.data = data
-		return data
+        found = self.results[POS]
+        found = found.sort(lemma, ascending=True)
+        found = found[lemma]
+        found = found[found > 0.0]
+        found_subset = found[1:10]
+        heads = found_subset.keys().tolist()
+        found_subset = found_subset.tolist()
 
-	def analyse(self, force = False, debug = False, path = None):
-		if self.from_pickle(path):
-			return self.results
+        return list(zip(heads, found_subset))
 
-		""" Run the algorythm on the corpus """
-		self.instance = self.algorythm(self.data)
-		self.instance.dictConvert()
-		self.instance.similarity()
+    def checkCacheAnalyse(self):
+        return False
 
-		self.results = self.instance.average
+    def generate(self, force=False):
+        """ Read and Generate the dictionaries """
+        data = {}
+        for lang in self.corpus.data:
+            data[lang] = self.corpus.data[lang].convert(force=force)
+        self.data = data
+        return data
 
-		self.to_pickle(debug = debug, path = path)
-		return self.results 
+    def analyse(self, force=False, debug=False, path=None):
+        if self.from_pickle(path):
+            return self.results
 
-	def from_pickle(self, path = None, debug = False):
-		if not path:
-			path = self.path + "/../Cache/"
-		path = "{0}OGL_{1}_{2}_average.pickle".format(path, self.algorythm.__name__, self.shelf.__name__)
+        """ Run the algorythm on the corpus """
+        self.instance = self.algorythm(self.data)
+        self.instance.dictConvert()
+        self.instance.similarity()
 
-		if not os.path.isfile(path):
-			return False
-			
-		with open(path, "rb") as f:
-			self.results = pickle.load(f)
-			return self.results
-		return False
+        self.results = self.instance.average
 
-	def to_pickle(self, path = None, debug = False):
-		if not path:
-			path = self.path + "/../Cache/"
-		path = "{0}OGL_{1}_{2}_average.pickle".format(path, self.algorythm.__name__, self.shelf.__name__)
+        self.to_pickle(debug=debug, path=path)
+        return self.results
 
-		with open(path, "wb") as f:
-			pickle.dump(self.results, f)
+    def from_pickle(self, path=None, debug=False):
+        if not path:
+            path = self.path + "/../Cache/"
+        path = "{0}OGL_{1}_{2}_average.pickle".format(path, self.algorythm.__name__, self.shelf.__name__)
 
-		if debug == True:
-			print ("Results saved to {0}".format(path))
+        if not os.path.isfile(path):
+            return False
 
+        with open(path, "rb") as f:
+            self.results = pickle.load(f)
+            return self.results
+        return False
 
-	def to_csv(self, path = None, debug = False):
-		for POS in self.results:
-			if not path:
-				path = self.path + "/Results/"
+    def to_pickle(self, path=None, debug=False):
+        if not path:
+            path = self.path + "/../Cache/"
+        path = "{0}OGL_{1}_{2}_average.pickle".format(path, self.algorythm.__name__, self.shelf.__name__)
 
-			path = "{0}OGL_{1}_{2}_{3}_average.csv".format(path, POS, self.algorythm.__name__, self.shelf.__name__)
+        with open(path, "wb") as f:
+            pickle.dump(self.results, f)
 
-			self.results[POS].to_csv(path)
-			if debug == True:
-				print ("Results saved to {0}".format(path))
+        if debug is True:
+            print ("Results saved to {0}".format(path))
+
+    def to_csv(self, path=None, debug=False):
+        for POS in self.results:
+            if not path:
+                path = self.path + "/Results/"
+
+            path = "{0}OGL_{1}_{2}_{3}_average.csv".format(path, POS, self.algorythm.__name__, self.shelf.__name__)
+
+            self.results[POS].to_csv(path)
+            if debug is True:
+                print ("Results saved to {0}".format(path))
--- Corpus/collatinus.py
+++ Corpus/collatinus.py
@@ -2,158 +2,158 @@
 # -*- coding: utf-8 -*-
 
 # Import sys for relative import
-import sys, os
+import sys
+import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 
 import regex as re
 import unicodedata
 from collections import defaultdict
-from string import punctuation
 
 from Corpus.dictionaries import Dictionary, Shelf
 
+
 class Collatinus(Dictionary):
-	def __init__(self, lang, *args, **kw):
-		super(self.__class__, self).__init__(*args, **kw)
+    def __init__(self, lang, *args, **kw):
+        super(self.__class__, self).__init__(*args, **kw)
 
-		self.isolang = {
-			"es" : "spa",
-			"ca" : "cat",
-			"uk" : "eng",
-			"fr" : "fre",
-			"de" : "ger",
-			"gl" : "gla",
-			"it" : "ita",
-			"pt" : "por"
-		}
+        self.isolang = {
+            "es": "spa",
+            "ca": "cat",
+            "uk": "eng",
+            "fr": "fre",
+            "de": "ger",
+            "gl": "gla",
+            "it": "ita",
+            "pt": "por"
+        }
 
-		self.collatinuslang = lang
-		self.sourcelang = "la"
-		self.targetlang = self.isolang[lang]
+        self.collatinuslang = lang
+        self.sourcelang = "la"
+        self.targetlang = self.isolang[lang]
 
-		self.root = os.path.dirname((os.path.abspath(__file__))) + "/../Copyrighted/collatinus/"
+        self.root = os.path.dirname((os.path.abspath(__file__))) + "/../Copyrighted/collatinus/"
 
-		self.latin = self.loadLatin()
+        self.latin = self.loadLatin()
 
-		self.file = self.root + "lemmata.{0}".format(self.collatinuslang)
+        self.file = self.root + "lemmata.{0}".format(self.collatinuslang)
 
-		#According to document mdlrad.la (Model radical) in collatinus.mdlrad
-		self.flexio = {
-			"0" : "N", #Noun
-			"1" : "N",
-			"2" : "N",
-			"3" : "N",
-			"4" : "N",
-			"5" : "N",
-			"6" : "N",
-			"7" : "N",
-			"8" : "N",
-			"9" : "N",
-			"10" : "N", 
-			"11" : "ADJ", #Adjectives
-			"12" : "ADJ",
-			"13" : "ADJ",
-			"14" : "ADJ",
-			"15" : "ADJ",
-			"16" : "ADJ",
-			"17" : "V", #Verb
-			"18" : "V",
-			"19" : "V",
-			"20" : "V",
-			"21" : "V",
-			"22" : "V",
-			"23" : "V",
-			"24" : "V",
-			"25" : "V",
-			"26" : "V",
-			"27" : "V",
-			"28" : "V",
-			"29" : "V",
-			"30" : "V",
-			"31" : "V",
-			"32" : "V",
-			"33" : "V",
-			"34" : "P", #Pron
-			"100" : "Greek", #Unknown as it has a greek translation
-			"101" : "Greek",
-			"102" : "Greek",
-			"103" : "Greek",
-			"104" : "Greek",
-			"105" : "Greek"
-		}
-		
-		self.senseSplitter = re.compile("(?:\:|\;|[0-9]+\.|(?:[\s]*\-)*[0-9]+[\s]*\-)")
-		self.getPath(self.__class__.__name__)
+        #According to document mdlrad.la (Model radical) in collatinus.mdlrad
+        self.flexio = {
+            "0": "N",  # Noun
+            "1": "N",
+            "2": "N",
+            "3": "N",
+            "4": "N",
+            "5": "N",
+            "6": "N",
+            "7": "N",
+            "8": "N",
+            "9": "N",
+            "10": "N",
+            "11": "ADJ",  # Adjectives
+            "12": "ADJ",
+            "13": "ADJ",
+            "14": "ADJ",
+            "15": "ADJ",
+            "16": "ADJ",
+            "17": "V",  # Verb
+            "18": "V",
+            "19": "V",
+            "20": "V",
+            "21": "V",
+            "22": "V",
+            "23": "V",
+            "24": "V",
+            "25": "V",
+            "26": "V",
+            "27": "V",
+            "28": "V",
+            "29": "V",
+            "30": "V",
+            "31": "V",
+            "32": "V",
+            "33": "V",
+            "34": "P",  # Pron
+            "100": "Greek",  # Unknown as it has a greek translation
+            "101": "Greek",
+            "102": "Greek",
+            "103": "Greek",
+            "104": "Greek",
+            "105": "Greek"
+        }
+        self.senseSplitter = re.compile("(?:\:|\;|[0-9]+\.|(?:[\s]*\-)*[0-9]+[\s]*\-)")
+        self.getPath(self.__class__.__name__)
 
-	def install(self):
-		raise NotImplementedError("Install is not installed")
+    def install(self):
+        raise NotImplementedError("Install is not installed")
 
-	def checkConverted(self):
-		raise NotImplementedError("CheckConverted is not implemented")
+    def checkConverted(self):
+        raise NotImplementedError("CheckConverted is not implemented")
 
-	def normalize(self, string):
-		if "=" in string:
-			string = string.split("=")[0]
-		sn = unicodedata.normalize('NFKD', string)
-		return ''.join(x for x in sn if unicodedata.category(x)[0] == 'L')
+    def normalize(self, string):
+        if "=" in string:
+            string = string.split("=")[0]
+        sn = unicodedata.normalize('NFKD', string)
+        return ''.join(x for x in sn if unicodedata.category(x)[0] == 'L')
 
-	def loadLatin(self):
-		data = {}
+    def loadLatin(self):
+        data = {}
 
-		with open(self.root + "lemmata.la") as f:
-			lines = [line for line in f.read().split("\n") if len(line)>0 and not line[0] == "!"]
-		
-		for line in lines:
-			elements = line.split("|")
-			lemma = elements[0]
-			lemma = self.normalize(lemma)
-			data[lemma] = elements[1]
+        with open(self.root + "lemmata.la") as f:
+            lines = [line for line in f.read().split("\n") if len(line) > 0 and not line[0] == "!"]
 
-		return data
+        for line in lines:
+            elements = line.split("|")
+            lemma = elements[0]
+            lemma = self.normalize(lemma)
+            data[lemma] = elements[1]
 
+        return data
 
-	def getPOS(self, lemma):
-		if lemma in self.latin:
-			flexioNumber = self.latin[lemma]
-			if flexioNumber in self.flexio:
-				return self.flexio[flexioNumber]
-		return "Unknown"
+    def getPOS(self, lemma):
+        if lemma in self.latin:
+            flexioNumber = self.latin[lemma]
+            if flexioNumber in self.flexio:
+                return self.flexio[flexioNumber]
+        return "Unknown"
 
-	def callback(self, force=False):
-		dictionaries = {
-			"V" : defaultdict(list),
-			"N" : defaultdict(list),
-			"ADJ" : defaultdict(list)
-		}
-		with open(self.file) as f:
-			lines = [line for line in f.read().split("\n") if len(line)>0 and not line[0] == "!"]
-		
-			for line in lines:
-				elements = line.split("|")
-				lemma = self.normalize(elements[0])
-				senses = elements[1]
-				senses = self.senseSplitter.split(senses)
-				POS = self.getPOS(lemma)
-				if POS in dictionaries:
-					for sense in senses:
-						if len(sense) > 0:
-							dictionaries[POS][lemma].append(self.removeStopwords(sense))
-					
-		return dictionaries
+    def callback(self, force=False):
+        dictionaries = {
+            "V": defaultdict(list),
+            "N": defaultdict(list),
+            "ADJ": defaultdict(list)
+        }
+        with open(self.file) as f:
+            lines = [line for line in f.read().split("\n") if len(line) > 0 and not line[0] == "!"]
 
-	def convert(self, force = False):
-		return self._convert(force = force, callback = self.callback)
+            for line in lines:
+                elements = line.split("|")
+                lemma = self.normalize(elements[0])
+                senses = elements[1]
+                senses = self.senseSplitter.split(senses)
+                POS = self.getPOS(lemma)
+                if POS in dictionaries:
+                    for sense in senses:
+                        if len(sense) > 0:
+                            dictionaries[POS][lemma].append(self.removeStopwords(sense))
+
+        return dictionaries
+
+    def convert(self, force=False):
+        return self._convert(force=force, callback=self.callback)
+
 
 class Collatini(Shelf):
-	"""A corpus representing every language available in Collatinus"""
-	def __init__(self):
-		data = {
-			"en" : Collatinus("uk"),
-			"fr" : Collatinus("fr"),
-			"de" : Collatinus("de"),
-			"ca" : Collatinus("ca"),
-			"gl" : Collatinus("gl"),
-			"it" : Collatinus("it"),
-			"pt" : Collatinus("pt")
-		}
-		super(Collatini, self).__init__(dictionaries = data)
+    """A corpus representing every language available in Collatinus"""
+    def __init__(self):
+        data = {
+            "en": Collatinus("uk"),
+            "fr": Collatinus("fr"),
+            "de": Collatinus("de"),
+            "ca": Collatinus("ca"),
+            "gl": Collatinus("gl"),
+            "it": Collatinus("it"),
+            "pt": Collatinus("pt")
+        }
+        super(Collatini, self).__init__(dictionaries=data)
